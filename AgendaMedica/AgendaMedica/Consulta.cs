c1c26e6f78013a1db2dba08d11d5ddec1307a7d3
simone-art <simoneperiodista3@gmail.com>
2020-04-08 18:15:36 -0300

    Código para programar cadastro de dados pessoais de usuário

---
+++ AgendaMedica/AgendaMedica/Consulta.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgendaMedica
+{
+    class Consulta
+    {
+
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+
+        public int CPF { get; set; }
+
+        public string Email { get; set; }
+
+        //public int CodigoUsuario { get; set; }
+
+        public void SalvarDadosUsuario()
+        {
+            Consulta.consultas.Add(this);
+        }
+
+        public static List<Consulta> consultas = new List<Consulta>();
+
+        public static List<Consulta> ListaConsultas()
+        {
+            return Consulta.consultas;
+
+        }
+    }
+}
---
+++ AgendaMedica/AgendaMedica/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace AgendaMedica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=================================================================");
+            Console.WriteLine("Bem-vindo a nossa plataforma de agendamento de consultas online");
+            Console.WriteLine("=================================================================");
+            Console.WriteLine("Escolha as seguintes opções");
+            Console.WriteLine("=================================================================");
+
+            while (true)
+            {
+                Console.WriteLine("1- Cadastrar Dados Pessoais");
+                Console.WriteLine("2- Escolha a especialidade");
+                Console.WriteLine("3- Escolha día e hora da consulta");
+                Console.WriteLine("4- Sair");
+                var opcoes = Convert.ToInt16(Console.ReadLine());
+                if (opcoes == 4) break;
+                Console.Clear();
+
+                switch(opcoes)
+                {
+                    case 1: 
+                Console.WriteLine("Cadastre o seu nome");
+                Console.ReadLine();
+                Console.WriteLine("Cadastre o seu sobrenome");
+                Console.ReadLine();
+                Console.WriteLine("Cadastre o seu CPF");
+                Console.ReadLine();
+                Console.WriteLine("Cadastre o seu email");
+                Console.ReadLine();
+                Console.WriteLine("Ingrese o código que voçê recebeu via email");
+                Console.ReadLine();
+               // MensagemConfirmacao();
+                        break;
+                }
+                Console.Clear();
+            }
+
+            //static void MensagemConfirmacao()
+            //{
+                //if (CodigoUsuario == 1234)
+                //{
+                   // Console.WriteLine("Cadastro feito com sucesso!");
+
+                //}
+            }
+        }
+    }
